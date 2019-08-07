df90be6b959c638682f90c1aade02594a0bc2f2b
roselandroche <r.landroche@gmail.com>
2019-08-07 19:45:35 -0400

    Added keydown and wheel events

--- js/index.js
+++ js/index.js
@@ -1,10 +1,39 @@
 // Your code goes here
-// Changed cursor for top nav
+// Mouseover
 const mainNav = document.querySelectorAll("a");
 mainNav.forEach(navItem => 
     navItem.addEventListener("mouseover", (event) => 
         navItem.style.cursor = "help"
     )
 );
+// Keydown
+const header = document.querySelector("header");
 
-const funBus = document.querySelector(".intro img");
+document.addEventListener("keydown", makeYellow);
+
+function makeYellow(event) {
+    if(event.keyCode === 89) {
+        header.style.backgroundColor = "yellow";
+    }
+}
+
+// Wheel
+const page = document.querySelector("body");
+
+document.addEventListener("wheel", zoom);
+
+function zoom(event) {
+    event.preventDefault;
+
+    scale += event.deltaY * -0.01;
+
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+
+  // Apply scale transform
+    page.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+
+// Drag
